70cb4fe565be9f37648134aca1adf85707ff340a
adityaverma0022 <138967791+adityaverma0022@users.noreply.github.com>
2023-08-31 16:25:21 +0530

    Update eth.sol

--- eth.sol
+++ eth.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract MyToken {
+
+    // public variables here
+     string public tokenname="ADITYA";
+     string public tokenabbr="AV";
+     uint public totalsupply=0;
+    // mapping variable here
+     mapping(address=>uint) public balances;
+
+    // mint function
+     function mint(address _address,uint _value) public 
+     {
+         balances[_address]+=_value;
+         totalsupply+=_value;
+     }
+    // burn function
+     function burn(address _address, uint _value) public {
+         if(balances[_address]>=_value)
+         {
+             totalsupply-=_value;
+             balances[_address]-=_value;
+
+
+         }}}
